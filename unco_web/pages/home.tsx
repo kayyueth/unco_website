2bceaed0152cf5920c095f6e55a807b73db474aa
K <yukiyukaiyue@gmail.com>
2024-12-13 15:54:50 +0000

    feat: home text

--- unco_web/pages/home.tsx
+++ unco_web/pages/home.tsx
@@ -5,6 +5,11 @@ export default function Homepage() {
             <img className="w-full" src='/home_bg.svg' />
             <div className="flex justify-center">
                 <img src='/home_flower.svg'/>
+                <div className="absolute text-center top-[1026px]">
+                    <h1>Uncommons</h1>
+                    <h1>IS A RESEARCHER GARDEN</h1>
+                    <p>FOR DECENTRALIZED THOUGHT AND TECHNO-SOCIAL PHILOSOPHIES</p>
+                </div>
             </div>
         </div>
     );
