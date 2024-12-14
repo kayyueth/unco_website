a2e0a6fbbf10da491a02632abf023cb06a999bc8
K <yukiyukaiyue@gmail.com>
2024-12-14 11:21:18 +0000

    feat: all fonts installed

--- unco_web/app/page.tsx
+++ unco_web/app/page.tsx
@@ -1,21 +1,29 @@
-import Homepage from "./home/page"
-import { Navbar } from "../components/layout/navbar"
-import { Footer } from "../components/layout/footer"
-import { Children } from "react";
+import { MoveRight } from 'lucide-react';
 
 export default function Home() {
   return (
     <div>
-      <img className="w-full" src='/home_bg.svg' />
-      <div className="flex justify-center">
-        <img src='/home_flower.svg'/>
-        <div className="absolute text-center mt-[76px]">
-          <h1>Uncommons</h1>
-          <h1>IS A RESEARCHER GARDEN</h1>
-          <p>FOR DECENTRALIZED THOUGHT AND TECHNO-SOCIAL PHILOSOPHIES</p>
+        <img className="w-full" src='/home_bg.svg' />
+        <div className="flex justify-center">
+            <img src='/home_flower.svg' className="flex mt-[80px]"/>
+            <div className="absolute text-center mt-[76px]">
+                <h1 className="text-4xl">Uncommons</h1>
+                <h3 className="text-xl">IS A RESEARCHER GARDEN</h3>
+                <p className="mt-10">FOR DECENTRALIZED THOUGHT</p>
+                <p>AND TECHNO-SOCIAL PHILOSOPHIES</p>
+                <div className='flex items-center justify-center mt-[100px] space-x-2'>
+                    <a
+                    href="https://uncommons.notion.site"
+                    className="flex justify-center underline"
+                    >
+                        Join us
+                    </a>
+                    <MoveRight 
+                        strokeWidth={1}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-
   );
 }
